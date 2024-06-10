1b8ade0061ef284ab88eac718af31f68674b01a2
da <>
2024-06-10 11:10:20 +0200

    rename some header files

--- basic.h
+++ basic.h
@@ -6,32 +6,26 @@
 /* TODO:
  * [-] dynamic array
  * [ ] hashtable
- * [ ] custom imgui
+ * [ ] strings & string api
  * [ ] (pseudo) random number generator
  * [ ] file & filepath operations
  * [ ] introspection (maybe with registries)
  * [ ] threads
- * [ ] json/csv/... parser
- * [ ] crossplatform audio library (miniaudio?)
- * [ ] custom, crossplatform opengl loader
- * [ ] custom, opengl 2d renderer
- * [ ] stb_img, stb_truetype
  *
  * for what else to add and how to do it, see
    https://nullprogram.com/blog/2023/02/11/
    https://nullprogram.com/blog/2023/02/15/
  */
 
-#include "basic_platform.h"
+#include "platform.h"
 
-#include "basic_buildtype.h" // depends on basic_platform.h  TODO maybe rename to basic_build.h
-
-#include "basic_math.h"      // depends on basic_platform.h
+#include "buildtype.h" /* depends on detected buildtype from platform.h */
+#include "maths.h"     /* depends on typedefs from platform.h */
 
 #ifdef BASIC_IMPLEMENTATION
   #define MEMORY_IMPLEMENTATION
 #endif
-#include "memory/memory.h"    // standalone
+#include "memory/memory.h"
 
 #ifdef BASIC_IMPLEMENTATION
   #define MEM_ARENA_IMPLEMENTATION
@@ -42,11 +36,11 @@
   #define MEM_ARENA_OS_RELEASE(ptr,size)  mem_release(ptr, size)
   #define MEM_ARENA_OS_DECOMMIT(ptr,size) mem_decommit(ptr, size)
 #endif
-#include "arena/arena.h"     // standalone
+#include "arena/arena.h"
 
-#include "basic_dynarr.h"    // depends on basic_memory.h
+#include "basic_dynarr.h"    /* depends on basic_memory.h */
 
-/* standalones: these do not depend on other headers */
-#include "basic_macro.h"
+/* standalones: these do not depend on other headers or on each other */
+#include "macros.h"
 #include "ext/utlist.h"      /* linked list macros (thirdparty) */
 #include "log/log.h"
